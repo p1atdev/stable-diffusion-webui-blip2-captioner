b7b1df0077654a3ed676be2363264cdb106300ff
Plat <git@p1at.dev>
2023-03-26 23:00:30 +0900

    fix: add pywintypes to install

--- install.py
+++ install.py
@@ -3,5 +3,11 @@ import launch
 if not launch.is_installed("lavis"):
     launch.run_pip(
         "install salesforce-lavis",
-        "requirements for BLIP2 captioner",
+        "requirements for BLIP2 captioner (lavis)",
+    )
+
+if not launch.is_installed("pywintypes"):
+    launch.run_pip(
+        "install pywintypes",
+        "requirements for BLIP2 captioner (pywintypes)",
     )
